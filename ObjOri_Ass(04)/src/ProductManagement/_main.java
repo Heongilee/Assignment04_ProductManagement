f5e5b4dd2c60ffc19055708b87f3a708f64a51d5
HeongiLee <gjsrl1@gmail.com>
2019-12-25 10:55:23 +0900

    user1_test

--- ObjOri_Ass(04)/src/ProductManagement/_main.java
+++ ObjOri_Ass(04)/src/ProductManagement/_main.java
@@ -1,7 +1,18 @@
 package ProductManagement;
 
+import javax.swing.JFrame;
+
+class MyFrame extends JFrame{
+	 public MyFrame() {
+		 super("04조_상품 관리 프로그램");
+		 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 
+		 setSize(800, 300);
+		 setVisible(true);
+	}
+}
 public class _main {
 	public static void main(String[] args) {
-		System.out.println("test");
+		new MyFrame();
 	}
 }

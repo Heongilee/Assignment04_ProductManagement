926363d7f0b0c90bd8d3f64f79e925a77f93de53
HeongiLee <gjsrl1@gmail.com>
2019-12-25 14:29:17 +0900

    191225 14:29

--- 04_productmanage.sql
+++ 04_productmanage.sql
@@ -4,6 +4,7 @@ ctrl + shift + Enter : 세미콜론으로 분리된 블럭 친 다중 쿼리문 
 ctrl + t : 새로운 쿼리 에디터 열기
 ctrl + shift + o : 저장된 스크립트(*.sql)파일 열기
 ctrl + b : 쿼리문 정리해줌.(개행이나 인덴팅 등.)			*/
+
 /*	테이블 삭제	*/
 DROP TABLE products;
 /*	테이블 모든 컬럼 조회	*/
@@ -16,5 +17,12 @@ CREATE TABLE 04_productmanage.products (
   MANUF VARCHAR(20) NOT NULL,
   PRIMARY KEY (ID))
 ENGINE = InnoDB
-auto_increment = 15
+auto_increment = 1
 DEFAULT CHARACTER SET = UTF8MB4;
+/*	삽입 삭제 수정 세이프 모드 해제 (테이블 처음 생성하면 이거 해줘야함)	*/
+SET SQL_SAFE_UPDATES = 0;
+
+/*	테이블의 모든 데이터 삭제	*/
+DELETE FROM PRODUCTS;
+/*	튜플 삽입	*/
+INSERT INTO 04_productmanage.products(PRODUCTS.NAME, PRODUCTS.PRICE, PRODUCTS.MANUF) VALUES('라면', 1500, '삼양');
